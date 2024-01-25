7e5c8387abc5745412d0f03eed70bed5092196b4
cjfwil <23485052+cjfwil@users.noreply.github.com>
2024-01-25 21:34:47 +0000

    msdn xaudio

--- WinMain.cpp
+++ WinMain.cpp
@@ -7,6 +7,9 @@
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
+#pragma comment(lib, "xaudio2.lib")
+#pragma comment(lib, "Ole32.lib")
+
 #include <windows.h>
 #include <stdbool.h>
 
@@ -18,9 +21,84 @@
 
 #include <stdio.h>
 
+#include <xaudio2.h>
+
 #include "src/Win32Window.hpp"
 #include "src/D3D11Renderer.hpp"
 
+//// XAUDIO HERE
+
+#define fourccRIFF 'FFIR'
+#define fourccDATA 'atad'
+#define fourccFMT ' tmf'
+#define fourccWAVE 'EVAW'
+#define fourccXWMA 'AMWX'
+#define fourccDPDS 'sdpd'
+
+HRESULT FindChunk(HANDLE hFile, DWORD fourcc, DWORD &dwChunkSize, DWORD &dwChunkDataPosition)
+{
+    HRESULT hr = S_OK;
+    if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, 0, NULL, FILE_BEGIN))
+        return HRESULT_FROM_WIN32(GetLastError());
+
+    DWORD dwChunkType;
+    DWORD dwChunkDataSize;
+    DWORD dwRIFFDataSize = 0;
+    DWORD dwFileType;
+    DWORD bytesRead = 0;
+    DWORD dwOffset = 0;
+
+    while (hr == S_OK)
+    {
+        DWORD dwRead;
+        if (0 == ReadFile(hFile, &dwChunkType, sizeof(DWORD), &dwRead, NULL))
+            hr = HRESULT_FROM_WIN32(GetLastError());
+
+        if (0 == ReadFile(hFile, &dwChunkDataSize, sizeof(DWORD), &dwRead, NULL))
+            hr = HRESULT_FROM_WIN32(GetLastError());
+
+        switch (dwChunkType)
+        {
+        case fourccRIFF:
+            dwRIFFDataSize = dwChunkDataSize;
+            dwChunkDataSize = 4;
+            if (0 == ReadFile(hFile, &dwFileType, sizeof(DWORD), &dwRead, NULL))
+                hr = HRESULT_FROM_WIN32(GetLastError());
+            break;
+
+        default:
+            if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, dwChunkDataSize, NULL, FILE_CURRENT))
+                return HRESULT_FROM_WIN32(GetLastError());
+        }
+
+        dwOffset += sizeof(DWORD) * 2;
+
+        if (dwChunkType == fourcc)
+        {
+            dwChunkSize = dwChunkDataSize;
+            dwChunkDataPosition = dwOffset;
+            return S_OK;
+        }
+
+        dwOffset += dwChunkDataSize;
+
+        if (bytesRead >= dwRIFFDataSize)
+            return S_FALSE;
+    }
+
+    return S_OK;
+}
+
+HRESULT ReadChunkData(HANDLE hFile, void *buffer, DWORD buffersize, DWORD bufferoffset)
+{
+    HRESULT hr = S_OK;
+    if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, bufferoffset, NULL, FILE_BEGIN))
+        return HRESULT_FROM_WIN32(GetLastError());
+    DWORD dwRead;
+    if (0 == ReadFile(hFile, buffer, buffersize, &dwRead, NULL))
+        hr = HRESULT_FROM_WIN32(GetLastError());
+    return hr;
+}
 
 INT WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -28,6 +106,81 @@ INT WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     ShowWindow(window.hwnd, SW_SHOWDEFAULT);
 
     D3D11Renderer renderer = D3D11Renderer(window.hwnd);
+
+    HRESULT hr = S_OK;
+
+    // XAUDIO2
+
+    hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+
+    IXAudio2 *pXAudio2 = nullptr;
+    hr = XAudio2Create(&pXAudio2, 0, XAUDIO2_DEFAULT_PROCESSOR);
+
+    IXAudio2MasteringVoice *pMasterVoice = nullptr;
+    hr = pXAudio2->CreateMasteringVoice(&pMasterVoice);
+
+    // load sound
+
+    // WAVEFORMATEX wfx = {};
+    // wfx.wFormatTag = WAVE_FORMAT_PCM;
+    // wfx.nChannels = 1; // 1: mono, 2: stereo
+    // wfx.nSamplesPerSec = 44100; //hz
+    // wfx.nAvgBytesPerSec = 1024; //todo: calculate properly
+    // wfx.wBitsPerSample = 8; // 8 or 16 for PCM, 32 for IEEE float
+    // wfx.nBlockAlign = (wfx.nChannels*wfx.wBitsPerSample)/8;
+    // wfx.cbSize = 0; //0 for PCM format
+
+    // XAUDIO2_BUFFER buffer;
+
+    WAVEFORMATEXTENSIBLE wfx = {0};
+    XAUDIO2_BUFFER buffer = {0};
+
+    TCHAR *strFileName = __TEXT("song.wav");
+    // Open the file
+    HANDLE hFile = CreateFile(
+        strFileName,
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        0,
+        NULL);
+
+    if (INVALID_HANDLE_VALUE == hFile)
+        return HRESULT_FROM_WIN32(GetLastError());
+
+    if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, 0, NULL, FILE_BEGIN))
+        return HRESULT_FROM_WIN32(GetLastError());
+
+    DWORD dwChunkSize;
+    DWORD dwChunkPosition;
+    // check the file type, should be fourccWAVE or 'XWMA'
+    FindChunk(hFile, fourccRIFF, dwChunkSize, dwChunkPosition);
+    DWORD filetype;
+    ReadChunkData(hFile, &filetype, sizeof(DWORD), dwChunkPosition);
+    if (filetype != fourccWAVE)
+        return S_FALSE;
+
+    FindChunk(hFile, fourccFMT, dwChunkSize, dwChunkPosition);
+    ReadChunkData(hFile, &wfx, dwChunkSize, dwChunkPosition);
+
+    // fill out the audio data buffer with the contents of the fourccDATA chunk
+    FindChunk(hFile, fourccDATA, dwChunkSize, dwChunkPosition);
+    BYTE *pDataBuffer = new BYTE[dwChunkSize];
+    ReadChunkData(hFile, pDataBuffer, dwChunkSize, dwChunkPosition);
+
+    buffer.AudioBytes = dwChunkSize;      // size of the audio buffer in bytes
+    buffer.pAudioData = pDataBuffer;      // buffer containing audio data
+    buffer.Flags = XAUDIO2_END_OF_STREAM; // tell the source voice not to expect any data after this buffer
+
+    // start xaudio
+    IXAudio2SourceVoice *pSourceVoice;
+
+    hr = pXAudio2->CreateSourceVoice(&pSourceVoice, (WAVEFORMATEX*)&wfx);
+
+    hr = pSourceVoice->SubmitSourceBuffer(&buffer);
+
+    hr = pSourceVoice->Start(0);
 
     bool quit = false;
     while (!quit)
