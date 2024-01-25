76f9f6056b757c3a95db389cc371c3ca7eb05f4e
cjfwil <23485052+cjfwil@users.noreply.github.com>
2024-01-25 13:20:17 +0000

    reorg

--- build_shaders.bat
+++ build_shaders.bat
@@ -1,3 +1,3 @@
 @echo off
-fxc /T vs_5_0 /E vs_main /Fo shaders/VShader.cso Shader.hlsl -nologo
-fxc /T ps_5_0 /E ps_main /Fo shaders/PShader.cso Shader.hlsl -nologo
+fxc /T vs_5_0 /E vs_main /Fo shaders/VShader.cso src/Shader.hlsl -nologo
+fxc /T ps_5_0 /E ps_main /Fo shaders/PShader.cso src/Shader.hlsl -nologo
