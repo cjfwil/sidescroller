63a691ae67f2d9c4d36faf554b57eb5aa40d7145
cjfwil <23485052+cjfwil@users.noreply.github.com>
2024-02-01 16:50:51 +0000

    initial commit space invaders

--- build_shaders.bat
+++ build_shaders.bat
@@ -1,4 +1,5 @@
 @echo off
+if  not exist shaders mkdir shaders
 fxc /T vs_5_0 /E vs_main /Fo shaders/VShader.cso src/ColourShader.hlsl -nologo
 fxc /T ps_5_0 /E ps_main /Fo shaders/PShader.cso src/ColourShader.hlsl -nologo
 
