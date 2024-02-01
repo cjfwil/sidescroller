5542e87b371abb29ead6fb12e8af10855db9f0d0
cjfwil <23485052+cjfwil@users.noreply.github.com>
2024-02-01 16:48:13 +0000

    rename

--- build_shaders.bat
+++ build_shaders.bat
@@ -1,6 +1,6 @@
 @echo off
-fxc /T vs_5_0 /E vs_main /Fo shaders/VShader.cso src/Shader.hlsl -nologo
-fxc /T ps_5_0 /E ps_main /Fo shaders/PShader.cso src/Shader.hlsl -nologo
+fxc /T vs_5_0 /E vs_main /Fo shaders/VShader.cso src/ColourShader.hlsl -nologo
+fxc /T ps_5_0 /E ps_main /Fo shaders/PShader.cso src/ColourShader.hlsl -nologo
 
 fxc /T vs_5_0 /E vs_main /Fo shaders/VShaderFont.cso src/FontRenderBMP.hlsl -nologo
 fxc /T ps_5_0 /E ps_main /Fo shaders/PShaderFont.cso src/FontRenderBMP.hlsl -nologo
